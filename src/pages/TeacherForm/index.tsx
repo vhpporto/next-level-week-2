53f9ca99d93787e6cfdbe256593bac85913f5993
Vitor Porto <vhpporto@gmail.com>
2020-08-05 17:35:49 -0700

    Upd

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PageHeader from "../../components/Header";
+
+import "./styles.css";
 
 function TeacherForm() {
-  return <h1>Teachers</h1>;
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que lega">teste</PageHeader>
+    </div>
+  );
 }
 
 export default TeacherForm;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,26 +1,61 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-import backIcon from "../../assets/images/icons/back.svg";
-import logoImg from "../../assets/images/logo.svg";
 
 import "./styles.css";
+import PageHeader from "../../components/Header";
+
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar" />
-          </Link>
-          <img src={logoImg} alt="Proffy" />
-        </div>
+      <PageHeader title="Estes são os proffys disponíveis">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" id="week_day" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
 
-        <div className="header-content">
-          <strong>Estes são os proffys disponíveis.</strong>
-        </div>
-      </header>
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img
+              src="https://avatars1.githubusercontent.com/u/45744241?s=460&u=abca30211f49d0a03e3a988c8b6e58ea1794cbd5&v=4"
+              alt="Vitor"
+            />
+            <div>
+              <strong>Vitor Porto</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Ddsahdusadhudsdusuh Dda isjds iasjd oidsaj oisajd
+            <br /> <br />
+            asoijasoidajsoasijdasasijsaoisajidjjdisaj ijdaso jdasi jdasio jsoaij
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 22,00</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp" />
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
